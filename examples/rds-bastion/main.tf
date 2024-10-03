b39aab6ec6799c253a8228d542549644ea0728d9
Stephen.James <stephen.james4@digital.justice.gov.uk>
2024-10-03 12:01:18 +0100

    Simple example of implementation.
    ND-533

---
+++ examples/rds-bastion/main.tf
@@ -0,0 +1,26 @@
+module "rds_admin_bastion_label" {
+  source       = "./modules/label"
+  service_name = "rds-admin-bastion"
+  owner_email  = var.owner_email
+}
+
+module "rds_admin_bastion" {
+  source                      = "git::https:github.com:ministryofjustice/diso-devops-module-ssm-bastion.git?depth=1"
+  ami_owners                  = ["${var.shared_services_account_id}"]
+  associate_public_ip_address = false
+  assume_role                 = local.s3-mojo_file_transfer_assume_role_arn
+  prefix                      = module.rds_admin_bastion_module_label.id
+  security_group_ids          = [module.admin.security_group_ids.admin_ecs]
+  subnets                     = module.admin_vpc.public_subnets
+  vpc_cidr_block              = module.admin_vpc.vpc.vpc_cidr_block
+  vpc_id                      = module.admin_vpc.vpc.vpc_id
+  tags                        = module.rds_admin_bastion_module_label.tags
+
+  providers = {
+    aws = aws.env
+  }
+
+  depends_on = [module.admin_vpc]
+  // Set in SSM parameter store, true or false to enable or disable this module.
+  count = var.enable_rds_admin_bastion == true ? 1 : 0
+}
